d7da758ee1cee876396ae83b9261266153755c98
Roger Farrell <127912262+RogerFarrell@users.noreply.github.com>
2025-05-07 19:51:30 -0500

    built a basic test of input/output

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main (main) where
 
-import Lib
-
 main :: IO ()
-main = someFunc
+main = do
+  let srcText = "The road goes ever on and on..."
+  putStrLn $ "Type: " ++ srcText
+  let mainloop = do
+        input <- getLine
+        if checkInput srcText input
+          then putStrLn "Correct"
+          else do
+            putStrLn "Try again"
+            mainloop
+  mainloop
+
+checkInput :: String -> String -> Bool
+checkInput src input = src == input
